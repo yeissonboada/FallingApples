ee6f1826fd0dd8f46aa1df2562786892978851ce
yeissonboada <yeisson.boada@talosdigital.com>
2021-08-18 11:23:05 -0500

    My first commit

---
+++ src/com/pluralsight/Main.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+
+public class Main {
+
+    public static void main(String[] args) {
+        for (int i =0; i < 10; i++) {
+            System.out.println("Testing code " + i);
+        }
+
+    }
+}
